bcc9ce4f6e7c79317f7df0e22ecbdb4a8b458b3c
rakibulhasan044 <rhrakib044@gmail.com>
2024-10-02 08:14:39 +0600

    added menu schema model

--- server/models/restaurant.model.ts
+++ server/models/restaurant.model.ts
@@ -16,38 +16,38 @@ export interface IRestaurantDocument extends IRestaurant, Document {
 }
 
 const restaurantSchema = new mongoose.Schema<IRestaurantDocument>({
-    user:{
-        type:mongoose.Schema.Types.ObjectId as any,
-        ref:"User",
-        required:true
+    user: {
+        type: mongoose.Schema.Types.ObjectId as any,
+        ref: "User",
+        required: true
     },
     restaurantName:{
-        type:String,
-        required:true
+        type: String,
+        required: true
     },
-    city:{
-        type:String,
-        required:true
+    city: {
+        type: String,
+        required: true
     },
-    country:{
-        type:String,
-        required:true
+    country: {
+        type: String,
+        required: true
     },
-    deliveryTime:{
-        type:Number,
-        required:true
+    deliveryTime: {
+        type: Number,
+        required: true
     },
-    cuisines:[{
-        type:String,
-        required:true
+    cuisines: [{
+        type: String,
+        required: true
     }],
-    menus:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Menu'
+    menus: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Menu'
     }],
-    imageUrl:{
-        type:String,
-        required:true
+    imageUrl: {
+        type: String,
+        required: true
     }
-},{timestamps:true});
+},{timestamps: true});
 export const Restaurant = mongoose.model("Restaurant", restaurantSchema);

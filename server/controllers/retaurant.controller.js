d7a1aea8dd7a0cfee989514c164691dc95d4835c
rakibulhasan044 <rhrakib044@gmail.com>
2024-10-03 06:50:08 +0600

    gitignore file added

--- server/controllers/retaurant.controller.js
+++ server/controllers/retaurant.controller.js
@@ -1,5 +1,6 @@
 import { Restaurant } from "../models/restaurant.model.js"
 import { Multer } from "Multer";
+import uploadImageOnCloudinary from "../utils/imageUpload.js";
 export const createRestaurant = async (req, res) => {
     try {
         const { restaurantName, city, country, price, deliveryTime, cuisines } = req.body;
@@ -18,6 +19,31 @@ export const createRestaurant = async (req, res) => {
             })
         }
         const imageUrl = await uploadImageOnCloudinary(file);
+        await Restaurant.create({
+            user: req.id,
+            restaurantName,
+            city,
+            country,
+            deliveryTime,
+            cuisines: JSON.parse(cuisines),
+            imageUrl
+        })
+
+        return res.status(201).json({
+            success: true,
+            message: "Restaurant Added"
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+export const getRestaurant = async (req, res) => {
+    try {
+        const restaurant = await Restaurant.find({user:user.id})
     } catch (error) {
         console.log(error);
         return res.status(500).json({

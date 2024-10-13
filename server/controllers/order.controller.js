8ab0ff75d724bdbf90d90f5d353ce558568bf1e7
rakibulhasan044 <rhrakib044@gmail.com>
2024-10-13 11:01:10 +0600

    bug fix and stripe webhook added

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -24,7 +24,7 @@ export const getOrders = async (req, res) => {
 export const createCheckoutSession = async (req, res) => {
     try {
         const checkoutSessionRequest = req.body;
-        const restaurant = await Restaurant.findById(checkoutSessionRequest.restaurantId).pupulate("menu");
+        const restaurant = await Restaurant.findById(checkoutSessionRequest.restaurantId).populate("menus");
         if(!restaurant) {
             return res.status(404).json({
                 success: false,
@@ -46,7 +46,7 @@ export const createCheckoutSession = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             shipping_address_collection: {
-                allowed_countries: ['GB', 'US', 'CA']
+                allowed_countries: ['GB', 'US', 'CA', 'BD']
             },
             line_items: lineItems,
             mode: 'payment',
@@ -71,20 +71,69 @@ export const createCheckoutSession = async (req, res) => {
     } catch (error) {
         console.log(error);
         return res.status(500).json({
-            message: "Enternal server error"
+            message: "Internal server error"
         })
     }
 }
 
+export const stripeWebhook = async (req, res) => {
+    let event;
+
+    try {
+        const signature = req.headers["stripe-signature"];
+
+        // Construct the payload string for verification
+        const payloadString = JSON.stringify(req.body, null, 2);
+        const secret = process.env.WEBHOOK_ENDPOINT_SECRET;
+
+        // Generate test header string for event construction
+        const header = stripe.webhooks.generateTestHeaderString({
+            payload: payloadString,
+            secret,
+        });
+
+        // Construct the event using the payload string and header
+        event = stripe.webhooks.constructEvent(payloadString, header, secret);
+    } catch (error) {
+        console.error('Webhook error:', error.message);
+        return res.status(400).send(`Webhook error: ${error.message}`);
+    }
+
+    // Handle the checkout session completed event
+    if (event.type === "checkout.session.completed") {
+        try {
+            const session = event.data.object ;
+            const order = await Order.findById(session.metadata?.orderId);
+
+            if (!order) {
+                return res.status(404).json({ message: "Order not found" });
+            }
+
+            // Update the order with the amount and status
+            if (session.amount_total) {
+                order.totalAmount = session.amount_total;
+            }
+            order.status = "confirmed";
+
+            await order.save();
+        } catch (error) {
+            console.error('Error handling event:', error);
+            return res.status(500).json({ message: "Internal Server Error" });
+        }
+    }
+    // Send a 200 response to acknowledge receipt of the event
+    res.status(200).send();
+};
+
 export const createLineItems = (checkoutSessionRequest, menuItems) => {
     // 1.create line items
     const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
-        const menuItem = menuItems.find((item) => item._id === cartItem.menuId)
-        if(!menuItem) throw new Error(`Menu item is not found`);
+        const menuItem = menuItems.find((item) => item._id.toString() === cartItem.menuId)
+        if(!menuItem) throw new Error(`Menu item id not found`);
 
         return{
             price_data: {
-                currency:"TK",
+                currency:"bdt",
                 product_data: {
                     name: menuItem.name,
                     images: [menuItem.image]
--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/isAuthenticated.js"
-import { createCheckoutSession, getOrders } from "../controllers/order.controller.js";
+import { createCheckoutSession, getOrders, stripeWebhook } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.route("/").get(isAuthenticated, getOrders);
 router.route("/checkout/create-checkout-session").post(isAuthenticated, createCheckoutSession);
-// router.route("/webhook").post();
+router.route("/webhook").post(express.raw({type: 'application/json'}), stripeWebhook);
 
 export default router;
